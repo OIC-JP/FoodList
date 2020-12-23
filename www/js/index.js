0b447acb3911970a3ea428565677c92332c71ae0
hinako <yukukosu03@gmail.com>
2020-12-23 21:35:52 +0900

    hinako

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,26 @@
 //ニフクラとの連携エリア＊＊＊データベース＊＊永野がやった＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
 // APIキーの設定とSDK初期化
 var ncmb = new NCMB("070898126fc8a57f789c8f7fa6dff549bba9773483c0a88ef8a506eed42a9c06","1f85ccf2e665bec807e342ff0f261dff3899338bf3b10b57875ae4906291224f");
-// 保存先クラスの作成
-var TestClass = ncmb.DataStore("TestClass");
-// 保存先クラスのインスタンスを生成
-var testClass = new TestClass();
-// 値を設定と保存
-testClass.set("message", "Hello, NCMB!")
-        .save()
-       .then(function(object){
-           // 保存に成功した場合の処理
-       })
-       .catch(function(err){
-           // 保存に失敗した場合の処理
-       });
+// // 保存先クラスの作成
+// var TestClass = ncmb.DataStore("TestClass");
+// // 保存先クラスのインスタンスを生成
+// var testClass = new TestClass();
+// // 値を設定と保存
+// testClass.set("message", "Hello, NCMB!")
+//         .save()
+//        .then(function(object){
+//            // 保存に成功した場合の処理
+//        })
+//        .catch(function(err){
+//            // 保存に失敗した場合の処理
+//        });
+
+//食材追加 クラスの作成
+var Food = ncmb.DataStore("Food");
+//クラスのインスタンスを生成
+var food = new Food();
+//値を保存
+food.set();
 
 ons.ready(function() {
   console.log("Onsen UI is ready!");
