0eae1d0ccae57a81db4b306ba62fa0c1ace64cd3
kozenimio <moon0621maron@gmail.com>
2021-01-13 13:21:37 +0900

    mio

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,8 @@
 //ニフクラとの連携エリア＊＊＊データベース永野＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
 
 // APIキーの設定とSDK初期化:
-var applicationKey = "070898126fc8a57f789c8f7fa6dff549bba9773483c0a88ef8a506eed42a9c06";
-var clientKey = "1f85ccf2e665bec807e342ff0f261dff3899338bf3b10b57875ae4906291224f";
+var applicationKey = "395d40b7250d31db288e826be0020a404383690e7d4e0fc37ef43a5bd61916a5";
+var clientKey = "50c00958b468ebe682b765254472f80f3e844f9d78c398dbd8ab3c0c1e05e4ce";
 var ncmb = new NCMB(applicationKey, clientKey);
 
 
@@ -126,7 +126,8 @@ var ncmb = new NCMB(applicationKey, clientKey);
                             var fileNameArray = fileName.split('_');
                             var li = document.createElement("li");
                             var p = document.createElement("p");
-                            p.innerHTML = "商品名："+fileNameArray[0]+"<br>個数："+fileNameArray[2];
+                            var day = fileNameArray[4].substr(0, 4)+"/"+fileNameArray[4].substr(4, 2)+"/"+fileNameArray[4].substr(6, 2);
+                            p.innerHTML = "商品名："+fileNameArray[0]+"<br>個数："+fileNameArray[2]+"<br>賞味期限："+day;
                             var c = "haiti"+" "+"すべて"+" "+fileNameArray[1];
                             li.setAttribute("class",c);
                             var img = document.createElement("img");
