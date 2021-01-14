85f9d8e2654e3dfd243082d49b66f38801872eac
kozenimio <moon0621maron@gmail.com>
2021-01-14 14:39:08 +0900

    mio

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,13 @@
 //ニフクラとの連携エリア＊＊＊データベース永野＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
-
 // APIキーの設定とSDK初期化:
 var applicationKey = "395d40b7250d31db288e826be0020a404383690e7d4e0fc37ef43a5bd61916a5";
 var clientKey = "50c00958b468ebe682b765254472f80f3e844f9d78c398dbd8ab3c0c1e05e4ce";
 var ncmb = new NCMB(applicationKey, clientKey);
-
 //起動時の処理
 ons.ready(function() {
   categoryCreate();
 });
-
-
-
 //＊＊＊食材の一覧＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
-
     var addpush = function(){
           var name = document.getElementById("name").value;
           var Name = encodeURIComponent(name);
@@ -32,10 +26,8 @@ ons.ready(function() {
           Buy_date[2] = Buy_date[2].padStart(2, '0');
           Expiration_date[1] = Expiration_date[1].padStart(2, '0');
           Expiration_date[2] = Expiration_date[2].padStart(2, '0');
-
           fileName = Name+"_"+Category+"_"+num+"_"+Buy_date[0]+Buy_date[1]+Buy_date[2]+"_"
                     +Expiration_date[0]+Expiration_date[1]+Expiration_date[2];
-
           ncmb.File.upload(fileName, fileData)
             .then(function(res){
               // アップロード後処理
@@ -52,11 +44,9 @@ ons.ready(function() {
               // エラー処理
               alert("エラー");
             });
-
           document.form.reset();
           document.getElementById("preview").src = "img/noimage.jpg";
     }
-
    //***食材表示
     function downloadImage(){
       document.getElementById("food-list").innerHTML = "";
@@ -66,14 +56,12 @@ ons.ready(function() {
           .then(function(results){
               // ファイルデータ取得成功時の処理
               console.log("ファイルデータ取得成功(allFile)");
-
               var promises = [];
               for (var i = 0; i < results.length; i++) {
                   var object = results[i];
                   // ファイルデータを元にPromiseを使って１件ずつ同期処理でファイルストアから画像を取得
                   promises.push(downloadFile(object, i)); 
               }
-
               /*** Promise ***/
               Promise.all(promises)
                   .then(function(results) {
@@ -95,54 +83,70 @@ ons.ready(function() {
               // loading の表示を終了
           });
     }
-
     //食材表示関数
+    var syoumi_arr = new Array();
+    var kounyu_arr = new Array();
+    var cnt=0;
+    var day1;
+    var day2;
     function downloadFile(object, i) {
         /*** Promise ***/
         return new Promise(function(resolve, reject) {        
             // 画像・作品名・作者名の表示先を指定
             var id = "syokuzai_" + i;
-
             // ファイルデータからファイル名を取得
             var fileName = object.fileName;
             var fileName_encode = encodeURI(fileName);
-
             // ファイルのダウンロード（データ形式をblobを指定）
             ncmb.File.download(fileName_encode, "blob")
-              .then(function(blob) {
-                  // ファイルダウンロード成功時の処理
-                  var reader = new FileReader();
-                  reader.onload = function(e) {
-                      // 画像URLを設定
-                      var dataUrl = reader.result;
-                      var fileNameArray = fileName.split('_');
-                      var li = document.createElement("li");
-                      var p = document.createElement("p");
-                      p.setAttribute("class","food-font");
-                      var day1 = fileNameArray[4].substr(0, 4)+"/"+fileNameArray[4].substr(4, 2)+"/"+fileNameArray[4].substr(6, 2); //賞味期限
-                      var day2 = fileNameArray[3].substr(0, 4)+"/"+fileNameArray[3].substr(4, 2)+"/"+fileNameArray[3].substr(6, 2); //購入日
-                      p.innerHTML = "食材名："+fileNameArray[0]+"<br>個数："+fileNameArray[2]+"<br>賞味期限："+day1+"<br>購入日："+day2+"<br>"+"<ons-button id='cancelbtn' onclick=\"cancelimg('"+fileName+"')\">"+"×"+"</ons-button>";
-                      var c = "haiti"+" "+"すべて"+" "+fileNameArray[1];
-                      li.setAttribute("class",c);
-                      var img = document.createElement("img");
-                      img.setAttribute("src",dataUrl);
-                      img.setAttribute("class","food-item");
-                      img.setAttribute("id",id);
-                      li.appendChild(img);
-                      li.appendChild(p); 
-                      document.getElementById("food-list").appendChild(li);
-                  }
-                  // ファイルリーダーにデータを渡す
-                  reader.readAsDataURL(blob);                       
-                  resolve("画像" +i); 
-              })
-              .catch(function(error) {
-                  // ファイルダウンロード失敗時の処理
-                  reject("画像" + i);
-              });
+                    .then(function(blob) {
+                        // ファイルダウンロード成功時の処理
+                        var reader = new FileReader();
+                        reader.onload = function(e) {
+                            // 画像URLを設定
+                            var dataUrl = reader.result;
+                            var fileNameArray = fileName.split('_');
+                            var li = document.createElement("li");
+                            var p = document.createElement("p");
+                            p.setAttribute("class","food-font");
+                            day1 = fileNameArray[4].substr(0, 4)+"/"+fileNameArray[4].substr(4, 2)+"/"+fileNameArray[4].substr(6, 2); //賞味期限
+                            //賞味期限をDate型に変換
+                            var day1Date = new Date (fileNameArray[4].substr(0, 4),fileNameArray[4].substr(4, 2)-1,fileNameArray[4].substr(6, 2));
+                            //残り日数を計算
+                            var termDay = (day1Date - end) / 86400000;
+                            var showDay = Math.ceil(termDay);//残り日数
+                            day2 = fileNameArray[3].substr(0, 4)+"/"+fileNameArray[3].substr(4, 2)+"/"+fileNameArray[3].substr(6, 2); //購入日
+                            if(showDay <= 3){
+                              p.setAttribute("style","color:red");
+                              p.innerHTML = "商品名："+fileNameArray[0]+"<br>個数："+fileNameArray[2]+"<br>賞味期限："+day1+"<br>購入日："+day2+"<br>"+"<ons-button id='cancelbtn' onclick=\"cancelimg('"+fileName+"')\">"+"×"+"</ons-button>";
+                            }else{
+                              p.innerHTML = "商品名："+fileNameArray[0]+"<br>個数："+fileNameArray[2]+"<br>賞味期限："+day1+"<br>購入日："+day2+"<br>"+"<ons-button id='cancelbtn' onclick=\"cancelimg('"+fileName+"')\">"+"×"+"</ons-button>";
+                            }
+                            var c = "haiti"+" "+"すべて"+" "+fileNameArray[1];
+                            li.setAttribute("class",c);
+                            var img = document.createElement("img");
+                            img.setAttribute("src",dataUrl);
+                            img.setAttribute("class","food-item");
+                            img.setAttribute("id",id);
+                            li.appendChild(img);
+                            li.appendChild(p); 
+                            document.getElementById("food-list").appendChild(li);
+                          
+                            //賞味期限と購入日を配列に格納
+                            syoumi_arr[cnt] += day1;
+                            kounyu_arr[cnt] += fileNameArray[3];                   
+                            cnt++;
+                        }
+                        // ファイルリーダーにデータを渡す
+                        reader.readAsDataURL(blob);                       
+                        resolve("画像" +i); 
+                    })
+                    .catch(function(error) {
+                        // ファイルダウンロード失敗時の処理
+                        reject("画像" + i);
+                    });
         });
     }
-
     //食材の削除
     function cancelimg(filename){
       var fa = filename.split('_');
@@ -158,29 +162,7 @@ ons.ready(function() {
         });
       }
     }
-
-   
-    //ニフクラからのデータ取得
-    ons.ready(function() {
-      var cg = ncmb.DataStore("Category");
-      var cg_item1 = "";
-      cg.order("createDate")
-        .fetchAll()
-        .then(function(results){
-          for (var i = 0; i < results.length; i++) {
-            var object = results[i];
-            cg_item1 += "<li class='menu-item'><a href='#"+object.category+"'>"+object.category+"</a></li>";
-          }
-          document.getElementById("menu-list").insertAdjacentHTML("beforeend",cg_item1);
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-    });
-
-
     //メニューを開く処理
-
     window.fn = {};
     window.fn.open1 = function() {
       var menu = document.getElementById('menu');
@@ -201,7 +183,6 @@ ons.ready(function() {
       selectboxCreate();
       accountbookCreate();
     };
-
     //ホーム画面のカテゴリー作成処理
     function categoryCreate(){
       var cg = ncmb.DataStore("Category");
@@ -235,11 +216,7 @@ ons.ready(function() {
         .catch(function(err){
             console.log(err);
         });
-
- 
     }
-
-
     //***カテゴリーごとの食材表示
     $(document).on('click', '.menu-list a', function() {
         var lists = $('.food-list li');
@@ -254,13 +231,34 @@ ons.ready(function() {
                 $(this).hide();
             }
         });
-    });   
+    });
+    //***並び替えメニュー  遠藤作業中　ＨＥＬＰ！！！！！！！
+    //賞味期限順
+    function syoumiFunk(){
+      
+          //ノードリストを取得
+          var syokuzai = document.getElementById("food-list");
+         
+          //並び替え
+          syoumi_arr.sort();//配列内の要素は並び替えできる
+  
+          for (var i=0;i<syoumi_arr.length;i++) {
+            alert(syoumi_arr[i]);
+             //syokuzai.appendChild(syokuzai.removeChild(syoumi_arr[i])); ?
+             //document.getElementById("food-list").appendChild(li); ?
+          }
+          //配列を並び替えたはいいけど、その後食材の再表示するのが分からん、、
+          //setTimeout("location.reload()",700);
+      }
+    
+    //購入日順
+    function kounyuFunk(){
+      kounyu_arr.sort();
+      alert(kounyu_arr);
+    }
     
     
-
-
 //＊＊＊食材の追加＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
-
     //選択した画像の表示
     function previewFile(obj) {
         var reader = new FileReader();
@@ -269,23 +267,16 @@ ons.ready(function() {
         });
         reader.readAsDataURL(obj.files[0]);
     }
-
     //画像clearボタンを押したとき
     function clearimg(){
       document.getElementById("preview").src = "img/noimage.jpg";
     }
-
     if (ons.platform.isIPhoneX()) {
       document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
       document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
     }
-
-
-
-
 //＊＊＊カテゴリー＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
-
-    //***カテゴリー追加
+    //カテゴリー追加
     function categoryadd(){
       var cate=prompt("追加するカテゴリーを入力してください");
       if(cate != null){
@@ -295,8 +286,7 @@ ons.ready(function() {
         setTimeout("location.reload()",700);
       }
     }
-
-    //***カテゴリー削除ダイアログの表示
+    //カテゴリー削除ダイアログの表示
     function showDialog(){
       var dialog = document.getElementById("dialog");
       if (dialog) {
@@ -310,7 +300,6 @@ ons.ready(function() {
         });
       }
     }
-
     //チェックボックスの作成
     function createcheckbox(){
       document.getElementById("dialog-item").innerHTML = "";
@@ -321,7 +310,7 @@ ons.ready(function() {
         .then(function(results){
           for (var i = 0; i < results.length; i++) {
             var object = results[i];
-            checkbox += "<br><input id='checkid' type='checkbox' name='check' value='"+object.category+"'>"+object.category;
+            checkbox += "<input type='checkbox' name='check' value='"+object.category+"'>"+object.category;
           }
           document.getElementById("dialog-item").insertAdjacentHTML("afterbegin",checkbox);
         })
@@ -329,7 +318,6 @@ ons.ready(function() {
             console.log(err);
         });
     }
-
     //カテゴリー削除処理
     function deleteDialog(){
       var cg = ncmb.DataStore("Category");
@@ -356,7 +344,6 @@ ons.ready(function() {
         setTimeout("location.reload()",700);
       }
     }
-
     //カテゴリー削除ダイアログの非表示
     function hideDialog() {
       document.getElementById("dialog").hide();
@@ -374,7 +361,7 @@ var end = new Date();
     var month2 = 0;  //先月の合計(last_month)
     var month3 = 0;  //先々月の合計(last_lastmonth)
     var last_month = new Date(end.getFullYear(), end.getMonth()-1, end.getDate());
-    var last_lastmonth = new Date(end.getFullYear(), end.getMonth()-2, end.getDate());
+    var last_lastmonth = new Date(end.getFullYear(), end.getMonth()-2, end.getDate())
     mn.order("createDate")
       .fetchAll()
       .then(function(results){
